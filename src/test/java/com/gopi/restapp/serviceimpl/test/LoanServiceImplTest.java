5f40ac999d4df5f41c791cd87cc468f21ba7fc41
Gopikrishnan.p <95020873+Gopikrishnan3007@users.noreply.github.com>
2024-07-30 16:32:15 +0530

    Update LoanServiceImplTest.java

--- src/test/java/com/gopi/restapp/serviceimpl/test/LoanServiceImplTest.java
+++ src/test/java/com/gopi/restapp/serviceimpl/test/LoanServiceImplTest.java
@@ -61,7 +61,7 @@ class LoanServiceImplTest {
 
 	@Test
 	void testDeleteLoan() {
-		assertEquals("Success",loanServiceImpl.deleteLoan(52));
+		assertEquals("Success",loanServiceImpl.deleteLoan(53));
 	}
 	
 	@Test
